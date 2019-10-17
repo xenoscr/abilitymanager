6a3b5474ec32ba3cc7e632bdcf33179ddbcba78e
Conor Richard <xenos@xenos-1.net>
2019-10-17 14:58:25 -0400

    Adding ability explosion

--- app/abilitymanager.py
+++ app/abilitymanager.py
@@ -35,11 +35,73 @@ class abilitymanager:
 		except:
 			pass
 
+	def explode_stockpile(self):
+		stockAbilities = []
+		if os.path.exists(self.stockPath):
+			self.log.debug(self.stockPath)
+			for root, dirs, files in os.walk(self.stockPath):
+				for procFile in files:
+					fullFile = os.path.join(root, procFile)
+					if os.path.splitext(fullFile)[-1].lower() == '.yml':
+						self.log.debug('Processing {}'.format(fullFile))
+						newAbility = {}
+						with open(fullFile, 'r') as yamlFile:
+							try:
+								stockData = yaml.load(yamlFile, Loader=yaml.Loader)
+							except:
+								continue
+						platformData = []
+						rawAbility = stockData[0]
+						rawPlatform = rawAbility['platforms']
+						for keyName in rawPlatform.keys():
+							for test in rawPlatform[keyName]:
+								newTest = {'executor': ''}
+								if 'command' in rawPlatform[keyName][test].keys():
+									newTest.update({ 'command': b64encode(rawPlatform[keyName][test]['command'].encode('utf8')).decode('utf-8') }) 
+								if 'cleanup' in rawPlatform[keyName][test].keys():
+									newTest.update({ 'cleanup': b64encode(rawPlatform[keyName][test]['cleanup'].encode('utf-8')).decode('utf-8') })
+								if 'parser' in rawPlatform[keyName][test].keys():
+									newTest.update({ 'parser': rawPlatform[keyName][test]['parser'] })
+								if (len(test.split(',')) > 1):
+									for subTest in test.split(','):
+										newTest['executor'] = subTest
+										platformData.append(newTest)
+								else:
+									newTest['executor'] = test
+									platformData.append(newTest)
+						newAbility = { 'id': rawAbility['id'], 'name': rawAbility['name'],
+							'description': rawAbility['description'], 'tactic': rawAbility['tactic'],
+							'technique': rawAbility['technique'], 'platforms': platformData }
+
+						stockAbilities.append(newAbility)
+		return { 'abilities': stockAbilities }
+
 	@template('abilitymanager.html')
 	async def landing(self, request):
 		await self.auth_svc.check_permission(request)
-		abilities = await self.data_svc.explode_abilities()
+		abilities = await self.explode_stockpile()
 		tactics = set([a['tactic'].lower() for a in abilities])
 		return { 'abilities': json.dumps(abilities), 'tactics': tactics }
 
-
+	async def rest_api(self, request):
+		self.log.debug('Starting Rest call.')
+		await self.auth_svc.check_permissions(request)
+		data = dict(await request.json())
+		index = data.pop('index')
+		self.log.debug('Index: {}'.format(index))
+		
+		options = dict(
+			PUT=dict(
+			),
+			POST=dict(
+				ac_ability=lambda d: self.explode_stockpile(**d),
+			),
+			DELETE=dict(
+			)
+		)
+		try:
+			output = await options[request.method][index](data)
+		except Exception as e:
+			self.log.debug('Stopped at api call.')
+			self.log.error(e)
+		return web.json_response(output)
